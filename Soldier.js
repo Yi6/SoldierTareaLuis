d6af562ec99f23eaca680ba714dc727520f15c80
Luis Diego Aguilar <luiscosconta@gmail.com>
2017-02-25 12:47:37 -0600

    Second Commint

--- Soldier.js
+++ Soldier.js
@@ -1,3 +1,13 @@
 /**
  * Created by LuisD on 25/02/2017.
  */
+"use strict";
+
+const state = require ('./state')
+const  Fsm = require ('./fsm')
+const eventEmitter = require ('./event-emiter')
+
+var inc = 0;
+
+/**/
+class
